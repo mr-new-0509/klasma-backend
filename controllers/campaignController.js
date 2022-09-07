621dd71a2428539ccb3bfe7d9fafd5fd47d2565a
mr-new-0509 <mr.new0509@gmail.com>
2022-09-07 12:41:23 +0900

    update create campaign

--- controllers/campaignController.js
+++ controllers/campaignController.js
@@ -1,8 +1,11 @@
-const { ID_OF_STATUS_APPROVED } = require("../utils/constants");
+const { ID_OF_STATUS_APPROVED, MESSAGE_SERVER_ERROR } = require("../utils/constants");
 const db = require("../utils/db");
+const { getCurrentDateTime } = require("../utils/functions");
 
+/** Create a new campaign */
 exports.createCampaign = async (req, res) => {
   const { goal_price, id_company, faqs } = req.body;
+  const currentDateTime = getCurrentDateTime();
   let sqlOfFields = '(';
   let sqlOfValues = '(';
   let _faqs = [];
@@ -15,8 +18,8 @@ exports.createCampaign = async (req, res) => {
   /* --------------------------------------- */
 
   /* ------------- Handle goal_price and id_company ------------ */
-  sqlOfFields += 'goal_price, id_company, id_status, ';
-  sqlOfValues += `${goal_price}, ${id_company}, ${ID_OF_STATUS_APPROVED}, `;
+  sqlOfFields += 'goal_price, id_company, id_status, created_at, ';
+  sqlOfValues += `${goal_price}, ${id_company}, ${ID_OF_STATUS_APPROVED}, "${currentDateTime}", `;
   delete req.body.goal_price;
   delete req.body.id_company;
   /* ----------------------------------------------------------- */
@@ -27,10 +30,10 @@ exports.createCampaign = async (req, res) => {
   fields.forEach((field, index) => {
     if (index == fields.length - 1) {
       sqlOfFields += `${field})`;
-      sqlOfValues += `'${req.body[field]}')`;
+      sqlOfValues += `"${String(req.body[field]).replace(/"/g, '\'\'')}")`;
     } else {
       sqlOfFields += `${field}, `;
-      sqlOfValues += `'${req.body[field]}', `;
+      sqlOfValues += `"${String(req.body[field]).replace(/"/g, '\'\'')}", `;
     }
   });
 
@@ -45,27 +48,35 @@ exports.createCampaign = async (req, res) => {
     for (let i = 0; i < _faqs.length; i += 1) {
       if (i === _faqs.length - 1) {
         sqlOfFaqValues += `(
-          '${_faqs[i].question}', 
-          '${_faqs[i].answer}', 
+          "${String(_faqs[i].question).replace(/"/g, '\'\'')}", 
+          "${String(_faqs[i].answer).replace(/"/g, '\'\'')}", 
           ${ID_OF_STATUS_APPROVED},
-          ${createdCampaign.insertId}
+          ${createdCampaign.insertId},
+          "${currentDateTime}"
         )`;
       } else {
         sqlOfFaqValues += `(
-          '${_faqs[i].question}', 
-          '${_faqs[i].answer}', 
+          "${String(_faqs[i].question).replace(/"/g, '\'\'')}", 
+          "${String(_faqs[i].answer).replace(/"/g, '\'\'')}", 
           ${ID_OF_STATUS_APPROVED},
-          ${createdCampaign.insertId}
+          ${createdCampaign.insertId},
+          "${currentDateTime}"
         ), `;
       }
     }
     await db.query(`
-      INSERT INTO faqs(question, answer, id_status, id_campaign) VALUES${sqlOfFaqValues};
+      INSERT INTO faqs(question, answer, id_status, id_campaign, created_at) VALUES${sqlOfFaqValues};
     `);
   }
   /* ----------------------------------------------------------- */
 
-  const campaigns = await db.query(`SELECT * FROM campaigns`);
+  return res.status(201).send('');
+};
 
-  return res.status(201).send(campaigns);
+/** Get campaigns of a company */
+exports.getCampaignsByCompanyId = (req, res) => {
+  const { id } = req.params;
+  db.query(`SELECT * FROM campaigns WHERE id_company = ${id}`)
+    .then(results => res.status(200).send(results))
+    .catch(error => res.status(500).send(MESSAGE_SERVER_ERROR));
 };
--- routes/campaignRoute.js
+++ routes/campaignRoute.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { createCampaign } = require('../controllers/campaignController');
+const { createCampaign, getCampaignsByCompanyId } = require('../controllers/campaignController');
 const authMiddleware = require('../middlewares/authMiddleware');
 
 router.post('/create', authMiddleware, createCampaign);
+router.get('/get-campaigns-by-company-id/:id', authMiddleware, getCampaignsByCompanyId);
 
 module.exports = router;
